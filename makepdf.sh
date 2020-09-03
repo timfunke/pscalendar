326f67b9f6e862e0834851cfb4635e871972ac8d
gbienhaus <guido.bienhaus@zooplus.com>
2020-09-03 11:15:44 +0200

    Added PNG creation

--- makepdf.sh
+++ makepdf.sh
@@ -7,3 +7,5 @@ if [ "$1" == "" ]; then
 fi
 # create a PDF file from the input PS file using ghostscript:
 gs -dNOPAUSE -sPAPERSIZE=a4 -sDEVICE=pdfwrite -sOutputFile=${1/.ps/.pdf} $1
+# gs -dNOPAUSE -sDEVICE=png16m -sOutputFile=calendar_2021.png -r120 $1
+# convert -rotate 90 calendar_2021.png calendar_2021r.png
